174f4433f44cac695bb2c688918bcb8c71844014
furti <daniel.furtlehner@gmx.net>
2015-10-24 17:01:59 +0200

    Added directorypicker to select directories for source and target

--- src/file-explorer.ts
+++ src/file-explorer.ts
@@ -1,27 +1,34 @@
 /// <reference path="../typings/angularjs/angular.d.ts"/>
 
 module SyncPlaylist {
+    export interface DirectoryChangeEvent {
+        directory: string
+    }
 
     class FileExplorerController {
-        public static $inject = ["$scope"];
+        public static $inject = ['$scope'];
         public directory: string;
 
         constructor($scope) {
             this.directory = $scope.directory;
         }
+
+        directoryChanged(newDirectory: string) {
+            this.directory = newDirectory;
+        }
     }
 
-    angular.module("syncPlaylist.directives", [])
-        .directive("fileExplorer", function() {
+    angular.module('syncPlaylist.directives', [])
+        .directive('fileExplorer', function() {
             return {
-                restrict: "E",
+                restrict: 'E',
                 scope: {
-                    directory: "=",
-                    onDirectoryChange: "&" //call wit onDirectoryChange({directory: newDir});
+                    directory: '=',
+                    onDirectoryChange: '&' //call wit onDirectoryChange({directory: newDir});
                 },
                 controller: FileExplorerController,
-                controllerAs: "fileExplorer",
-                templateUrl: "templates/file-explorer.html"
+                controllerAs: 'fileExplorer',
+                templateUrl: 'templates/file-explorer.html'
             };
         });
 }
